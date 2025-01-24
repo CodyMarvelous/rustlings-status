289cf3bd9f3cd5282953cec8c6aa2f4cae18f402
JustlySold <jonatansvensson00@gmail.com>
2025-01-25 00:42:05 +0100

    implementing will be common. It is interesting but at te end of the day one can consider it a contract that components will have those traits

--- exercises/15_traits/traits3.rs
+++ exercises/15_traits/traits3.rs
@@ -3,7 +3,14 @@ trait Licensed {
     // implementors like the two structs below can share that default behavior
     // without repeating the function.
     // The default license information should be the string "Default license".
-    fn licensing_info(&self) -> String;
+    fn licensing_info(&self) -> String{
+        String::from("Default license")
+    }
+    fn return_damn(&self) -> String{
+        String::from("Damn")
+    }
+    fn return_damned(&self) -> String;
+    
 }
 
 struct SomeSoftware {
@@ -14,8 +21,16 @@ struct OtherSoftware {
     version_number: String,
 }
 
-impl Licensed for SomeSoftware {} // Don't edit this line.
-impl Licensed for OtherSoftware {} // Don't edit this line.
+impl Licensed for SomeSoftware {
+    fn return_damned(&self) -> String{
+        String::from("Damned")
+    }
+} // Don't edit this line.
+impl Licensed for OtherSoftware {
+    fn return_damned(&self) -> String{
+        String::from("Damned")
+    }
+} // Don't edit this line.
 
 fn main() {
     // You can optionally experiment here.
@@ -34,5 +49,11 @@ mod tests {
         };
         assert_eq!(some_software.licensing_info(), licensing_info);
         assert_eq!(other_software.licensing_info(), licensing_info);
+    }
+    #[test]
+    fn is_damn(){
+        let damn_info = "Damn";
+        let some_software = SomeSoftware { version_number: 1};
+        assert_eq!(some_software.return_damn(), damn_info);
     }
 }
